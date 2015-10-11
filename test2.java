7b9869a8bf186a96059aec5f366113bbc21db317
yuya okuda <14ec033@ms.dendai.ac.jp>
2015-10-11 22:51:35 +0900

    拡張for文
    教科書

---
+++ test2.java
@@ -0,0 +1,16 @@
+﻿//拡張for文　例
+class test2 {
+	public static void main(String args[]){
+		double[]a={1.0,2.0,3.0,4.0,5.0};//配列
+		
+		for(int i=0;i<a.length;i++)//配列の中身の表示
+			System.out.println("a["+i+"]="+a[i]);
+		double sum=0;//合計
+		for(double i:a)//拡張for文
+			sum+=i;
+		
+		System.out.println("全要素の和は"+sum+"です");//合計の表示
+	}
+}
+
+
