78e186d63669dee561e32af1998d0a6705a6cf12
yuya okuda <14ec033@ms.dendai.ac.jp>
2015-10-13 23:53:15 +0900

    配列二分探索
    教科書

---
+++ StringBinarySearch.java
@@ -0,0 +1,22 @@
+﻿import java.util.Arrays;
+import java.util.Scanner;
+
+class StringBinarySearch{
+	
+	public static void main(String[] args){
+		Scanner stdIn=new Scanner(System.in);
+		String[]x={
+			"abstract","assert","boolean","break","byte",
+			"case","catch","char","double","enum","final","if","void"
+		};
+		System.out.print("何を探しますか：");
+		String ky=stdIn.next();
+		
+		int idx=Arrays.binarySearch(x,ky);
+		
+		if(idx<0)
+			System.out.println("そのキーワードは存在しません");
+		else
+			System.out.println("それはx["+idx+"]にあります");
+	}
+}
