6510ad6dba08cfdb23580d28c10b50e25ce06455
yuya okuda <14ec033@ms.dendai.ac.jp>
2015-10-14 06:52:09 +0900

    オブジェクトの配列からの探索
    教科書

--- StringBinarySearch.java
+++ StringBinarySearch.java
@@ -1,22 +1,23 @@
-﻿import java.util.Arrays;
+﻿//文字配列からの探索
+import java.util.Arrays;
 import java.util.Scanner;
 
 class StringBinarySearch{
 	
 	public static void main(String[] args){
 		Scanner stdIn=new Scanner(System.in);
-		String[]x={
+		String[]x={//キーワード(アルファベット順)
 			"abstract","assert","boolean","break","byte",
 			"case","catch","char","double","enum","final","if","void"
 		};
-		System.out.print("何を探しますか：");
-		String ky=stdIn.next();
+		System.out.print("何を探しますか：");//キーの読み込み
+		String ky=stdIn.next();//キーの入力
 		
-		int idx=Arrays.binarySearch(x,ky);
+		int idx=Arrays.binarySearch(x,ky);//配列xから値がkyの要素を探索
 		
 		if(idx<0)
-			System.out.println("そのキーワードは存在しません");
+			System.out.println("そのキーワードは存在しません");//存在しない場合
 		else
-			System.out.println("それはx["+idx+"]にあります");
+			System.out.println("それはx["+idx+"]にあります");//存在する場合
 	}
 }
