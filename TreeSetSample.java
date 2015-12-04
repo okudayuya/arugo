65defb07d3c7e4a74a74d62a8aefdcc471f0f43a
yuya okuda <14ec033@ms.dendai.ac.jp>
2015-12-04 23:41:08 +0900

    TreeSetクラスの体験
    授業で取り扱ったプログラム

---
+++ TreeSetSample.java
@@ -0,0 +1,32 @@
+﻿// Setの実装であるTreeSetクラスについて体験する
+// 自動的に要素がソートされる
+import java.util.*;
+public class TreeSetSample {
+	public static void main(String[] args){
+		// Setの実装であるHashクラスのインスタンス生成，今回は文字列型
+		// ジェネリクス（総称型）を使っている（復習）
+		Set<String> set = new TreeSet<String>();
+		set.add("C"); // 要素の追加
+		set.add("A");
+		set.add("B");
+		set.add("B"); // 同じ要素を追加しても要素は重複されない
+		
+		for(String item:set){ // 要素の一覧を表示； 拡張For文（復習）
+			System.out.println(item);
+		}
+		System.out.println("");
+		
+		Iterator<String> iter = set.iterator(); // イテレータを使った走査
+		while(iter.hasNext()){
+			String item = iter.next();
+			System.out.println(item);
+		}
+		System.out.println("");
+		
+		set.remove("A"); // 要素の削除
+		
+		for(String item:set){ // 要素の一覧を表示
+			System.out.println(item);
+		}
+	}
+}
