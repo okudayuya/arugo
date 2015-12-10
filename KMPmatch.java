e16b47d0ead4d57c44b9fa41a8cd0376c8c0347a
yuya okuda <14ec033@ms.dendai.ac.jp>
2015-12-10 09:08:24 +0900

    KMP法による文字列探索　メソッド
    教科書

---
+++ KMPmatch.java
@@ -0,0 +1,35 @@
+﻿//KMPによる文字列探索
+	static int kmpMatch(String txt,String pat){
+		int pt=1;							//txtをなぞるカーソル
+		int pp=0;							//patをなぞるカーソル
+		int[] skip=new int[pat.length()+1];	//スキップテーブル
+		
+		//スキップテーブルの作成
+		skip[pt]=0;
+		while (pt !=pat.length()){
+			if(pat.charAt(pt)==pat.charAt(pp))
+				skip[++pt]=++p;
+			else if(pp==0)
+				skip[++pt]=pp;
+			else
+				pp=skip[pp];
+		}
+		
+		//探索
+		pt=pp=0;
+		while(pt !=txt.length()&&pp !=pat.length()){
+			if(txt.charAt(pp)==pat.charat(pp)){
+				pt++;
+				pp++;
+			}
+			else if(pp==0)
+				pt++;
+			else
+				pp=skip[pp];
+		}
+		
+		if(pp==pat.length()
+			return pt-pp;
+		return -1;
+	}
+	
