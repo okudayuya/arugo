5e015838f8dc7a71df08d73a42e1d09e70174d55
yuya okuda <14ec033@ms.dendai.ac.jp>
2016-01-04 14:48:43 +0900

    階乗を求めるプログラム
    再帰的

---
+++ fact.java
@@ -0,0 +1,12 @@
+﻿//階乗を求めるプログラム
+public class fact {
+  static int fact(int n){//再帰プログラムの使用
+    return n == 0 ? 1 : n * fact(n - 1);
+  }
+
+  public static void main(String[] args){
+    for(int i = 0; i < 13; i++){
+      System.out.println(i + " : " + fact(i));
+    }
+  }
+}
