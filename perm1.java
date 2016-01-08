0b1e3ef48256b1118425c3f92bcee46e84060d61
yuya okuda <14ec033@ms.dendai.ac.jp>
2016-01-08 20:43:36 +0900

    順列の生成　再帰
    自習

---
+++ perm1.java
@@ -0,0 +1,26 @@
+﻿public class perm1 {
+  static void print_perm(int[] perm){
+    for(int x: perm){
+      System.out.print(x + " ");
+    }
+    System.out.println();
+  }
+  
+  static void make_perm(int n, int[] perm, boolean[] flag){
+    if(n == perm.length){
+      print_perm(perm);
+    } else {
+      for(int i = 1; i <= perm.length; i++){
+        if(flag[i]) continue;
+        perm[n] = i;
+        flag[i] = true;
+        make_perm(n + 1, perm, flag);
+        flag[i] = false;
+      }
+    }
+  }
+
+  public static void main(String[] args){
+    make_perm(0, new int [4], new boolean [5]);
+  }
+}
