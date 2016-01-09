5c42c537b14ac3f6fcacba2199e06478f54a9f5a
yuya okuda <14ec033@ms.dendai.ac.jp>
2016-01-10 01:01:15 +0900

    Collections.sortを使った文字列ソート
    自習

---
+++ CollectionSample.java
@@ -0,0 +1,18 @@
+﻿//Collections.sortを使った文字列ソート
+import java.util.*;
+public class CollectionSample{
+	public static void main(String[] args){
+		//ジェネリクスの使用
+		List<String> array= new ArrayList<String>();
+		array.add("s");//要素の追加
+		array.add("a");
+		array.add("d");
+		array.add("r");
+		array.add("w");
+		array.add("e");
+		Collections.sort(array);
+		for(String string:array){//拡張for文
+			System.out.println(string);
+		}
+	}
+}
