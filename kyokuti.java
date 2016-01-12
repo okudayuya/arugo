510002f43d80b5d25b2da366233bf4c67107a65f
yuya okuda <14ec033@ms.dendai.ac.jp>
2016-01-12 20:23:42 +0900

    局地変数の有効範囲
    自習

---
+++ kyokuti.java
@@ -0,0 +1,21 @@
+﻿//局地変数の有効範囲
+public class kyokuti {
+  public static void main(String[] args){
+    int x = 1;
+    {
+      int y = 2;
+      {
+        int z = 3;
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+      }
+      System.out.println(x);
+      System.out.println(y);
+      // System.out.println(z);  z は範囲外 (コンパイルエラー)
+    }
+    System.out.println(x);
+    // System.out.println(y);    y は範囲外 (コンパイルエラー)
+    // System.out.println(z);    z は範囲外 (コンパイルエラー)
+  }
+}
